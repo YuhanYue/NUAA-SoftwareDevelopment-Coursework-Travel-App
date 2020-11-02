95eb209628e1e584221524d0ac6c7d8f35b22fc2
Hope <yuhan0810@outlook.com>
2020-11-02 09:57:35 +0800

    Update

--- Backend/AwesomeProject/app.js
+++ Backend/AwesomeProject/app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+/*var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -39,3 +39,38 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+*/
+
+var express = require('express');   //引入express模块
+var mysql = require('mysql');     //引入mysql模块
+var app = express();        //创建express的实例
+ 
+var connection = mysql.createConnection({      //创建mysql实例
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'123456',
+    database:'TravelApp'
+});
+connection.connect();
+var sql = 'SELECT * FROM Admin';
+
+connection.connect();
+var sql = 'SELECT * FROM websites';
+var str = " ";
+connection.query(sql, function (err,result) {
+    if(err){
+        console.log('[SELECT ERROR]:',err.message);
+    }
+    str = JSON.stringify(result);
+    //数据库查询的数据保存在result中，但浏览器并不能直接读取result中的结果，因此需要用JSON进行解析
+    //console.log(result);   //数据库查询结果返回到result中
+    console.log(str);
+  });
+app.get('/',function (req,res) {
+    res.send('Hello,myServer');  //服务器响应请求
+});
+connection.end();
+app.listen(3000,function () {    //监听3000端口
+    console.log('Server running at 3000 port');
+});
